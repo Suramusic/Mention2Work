097f9e9e9f54767a6cc60375167b9340235fb44d
Suramusic <123370264+Suramusic@users.noreply.github.com>
2023-03-04 08:55:44 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -136,7 +136,7 @@ async def mentionall(event):
             break
         usrnum += 1
         usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-        if usrnum == 5:
+        if usrnum == 1:
             if mode == "text_on_cmd":
                 txt = f"{usrtxt}\n\n{msg}"
                 await client.send_message(chat_id, txt)
@@ -196,7 +196,7 @@ async def _(event):
             break
         usrnum += 1
         usrtxt += f" \n [{x.first_name}](tg://user?id={x.id})"
-        if usrnum == 5:
+        if usrnum == 1:
             if mode == "text_on_cmd":
                 txt = f"{usrtxt}\n\n{msg}"
                 await client.send_message(chat_id, txt)
