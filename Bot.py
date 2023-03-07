868f9c373470e288d4d9b607db5b631976806aa9
Suramusic <123370264+Suramusic@users.noreply.github.com>
2023-03-07 10:51:22 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -91,8 +91,8 @@ async def help(event):
 
 
 @client.on(events.NewMessage(pattern="^/mentionall ?(.*)"))
-@client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
 @client.on(events.NewMessage(pattern="^/@all ?(.*)"))
+@client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
 @client.on(events.NewMessage(pattern="^/#all ?(.*)"))
 async def mentionall(event):
     chat_id = event.chat_id
